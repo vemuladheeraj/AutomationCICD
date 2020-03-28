1a32f7074be1a34d77b2ccb87034cd50b2b7cc9d
Dheeraj Vemula <vemula.dheeraj@outlook.com>
2020-03-28 13:32:27 +0530

    All Working changes

---
+++ CICDTest/CICDTest/TestMain.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CICDTest
+{
+    [TestFixture]
+    [Category("Nunit Demonstration")]
+
+    public class TestMain : ProjectTestBase
+    {
+        [Test]
+        public void BasicTest()
+        {
+            //Need to validate
+            var context = this.DriverContext;
+            var OpenPage = new WikiPage(context);
+            OpenPage.OpenPage().EnterSearchValue();
+        }
+
+    }
+}
---
+++ CICDTest/CICDTest/WikiPage.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CICDTest.Helpers;
+using CICDTest.Page_Objects;
+using CICDTest.Types;
+
+namespace CICDTest
+{
+    public partial class WikiPage : ProjectBase
+    {
+        //  private DriverContext driverContext;
+
+        public WikiPage(DriverContext driverContext) : base(driverContext)
+        {
+
+        }
+
+        private readonly ElementLocator
+            googleSearch = new ElementLocator(Locator.Xpath, "//input[contains(@class,'gLFyf gsfi')]");
+        //basicAuthLink = new ElementLocator(Locator.XPath, "//a[contains(text(),'Auth')]"),
+        //dropdownPageByLinkTextLocator = new ElementLocator(Locator.LinkText, "Dropdown"),
+        //partialLinkTextLocator = new ElementLocator(Locator.PartialLinkText, "Drag");
+
+        private readonly By search = By.XPath("//input[contains(@class,'gLFyf gsfi')]");
+        private readonly By searchBtn = By.Name("btnK");
+        private readonly By WellsFargoLink = By.PartialLinkText("Wells Fargo - Wikipedia");
+
+
+        public WikiPage OpenPage()
+        {
+            var url = BaseConfiguration.GetUrlValue;
+            this.Driver.NavigateTo(new Uri(url));
+            return this;
+        }
+
+
+        public WikiPage EnterSearchValue()
+        {
+            this.Driver.FindElement(search).SendKeys("WellsFargo");
+            System.Threading.Thread.Sleep(2000);
+            this.Driver.FindElements(searchBtn)[0].Click();
+
+            return this;
+        }
+
+        public WikiPage SeachFOrWellFargoWIKILink()
+        {
+            this.Driver.FindElement(WellsFargoLink).Click();
+            return this;
+        }
+
+
+
+
+    }
+}
